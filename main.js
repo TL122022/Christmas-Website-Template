056f234adbbe08ddadafb551333fcc0e4a564735
TL12 <101867993+TL122022@users.noreply.github.com>
2022-11-13 13:33:31 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+/* ------------- PRELOADER ------------- */
+
+/* ------------- SCROLL TO TOP BUTTON CHANGE ------------- */
+
+/* ------------- CHANGE HEADER ON SCROLL ------------- */
+
+/* ------------- TOGGLE NAVIGATION ------------- */
+
+/* HIDE NAV WHEN GO TO SECTION */
+
+
+/* ------------- POP UP MODALS ------------- */
+
+/* ------- 1ST POP UP ------- */
+
+/* ------- 2ND POP UP ------- */
+
+/* ------- 3RD POP UP ------- */
+
+/* ------- 4TH POP UP ------- */
+
+
+/* ------------- SWIPER ------------- */
+
+/* ------------- PARTY SIGNUP FORM ------------- */
+
+/* ------------- SCROLL TO SECTION------------- */
+
+/* ------------- SLIDE IN WHEN IN VIEW ------------- */
+
+/* ------------- TOGGLE LIGHT/DARK MODE ------------- */
